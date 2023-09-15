a3a7f319b5d3fee443fecde3601f2076f8b2fd62
mzvonar <riffmaker@gmail.com>
2023-09-15 11:08:54 +0200

    Check image size on upload

--- netlify/functions/upload-image.ts
+++ netlify/functions/upload-image.ts
@@ -2,9 +2,13 @@ import crypto from 'crypto';
 import type { Handler, HandlerEvent } from "@netlify/functions";
 import axios from 'axios';
 import {v2 as cloudinary} from 'cloudinary';
+import getImageSize from 'image-size';
 
 const RECAPTCHA_SECRET = process.env.GOOGLE_RECAPTCHA_SECRET;
 const IMAGE_CDN_FOLDER = process.env.VITE_IMAGE_CDN_FOLDER || 'prod';
+
+const WIDTH = 1000;
+const HEIGHT = 500;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,6 +18,13 @@ cloudinary.config({
 
 function getImageHash(imageBase64: string) {
   return crypto.createHash('md5').update(Buffer.from(imageBase64, 'base64')).digest('hex');
+}
+
+function checkImageDimensions(imageBase64: string) {
+  const image = Buffer.from(imageBase64.substr(23), 'base64');
+  const dimensions = getImageSize(image);
+
+  return dimensions.width === WIDTH && dimensions.height === HEIGHT;
 }
 
 export const handler: Handler = async (event: HandlerEvent) => {
@@ -28,7 +39,16 @@ export const handler: Handler = async (event: HandlerEvent) => {
     }
 
     const { recaptchaToken, imageBase64 } = JSON.parse(event.body);
-    
+
+    if(!checkImageDimensions(imageBase64)) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'Bad request',
+        }),
+      };
+    }
+
     // Verify reCAPTCHA token with Google
     const ipAddress = event.headers['client-ip'];
     const recaptchaResponse = await axios.post(
@@ -45,7 +65,7 @@ export const handler: Handler = async (event: HandlerEvent) => {
         folder: `prazdne-hesla/${IMAGE_CDN_FOLDER}`, // Optional: Specify a folder in Cloudinary for uploads
         public_id: imageHash,
       });
-console.log('uploadResponse: ', uploadResponse);
+
       return {
         statusCode: 200,
         body: JSON.stringify({
@@ -58,7 +78,7 @@ console.log('uploadResponse: ', uploadResponse);
       // reCAPTCHA verification failed
       return {
         statusCode: 403,
-        body: JSON.stringify({ message: 'reCAPTCHA verification failed' }),
+        body: JSON.stringify({ message: 'Forbidden' }),
       };
     }
   }
